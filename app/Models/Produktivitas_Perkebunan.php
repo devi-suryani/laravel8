0e7ba2bf73ce4d52d5713efaf474e040eac05cf5
devi-suryani <devidamaris07@gmail.com>
2020-12-19 16:55:10 +0800

    project pertama

---
+++ app/Models/Produktivitas_Perkebunan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produktivitas_Perkebunan extends Model
+{
+    use HasFactory;
+    protected $table = 'produktivitas_perkebunan';
+
+    protected $filllable = ['kecamatan_id','tanaman_perkebunan_id','nilai'] ;
+}
---
+++ resources/views/tanaman/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+<div class="content">
+<div class="card">
+<div class="card-body"> 
+{!! Form::open(['route'=> 'tanaman.store'])!!}
+    <div class="body m-10">
+        <div class="form-group">
+        {!! Form::label('nama','Nama'); !!}
+        {!! Form::text('nama','',['class' => 'form-control','placeholder'=>'nama tanaman']); !!}
+        </div>
+        <button class="btn btn-success">Simpan</button>
+        <a href="{!! route('tanaman.index') !!}" class="btn btn-danger">Cancel</a> 
+        </div>
+    {!! Form::close() !!}
+    </div>
+    </div>
+    </div>
+@endsection
